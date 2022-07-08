31426f7476d6c4118ae3d45cb8f5b09595ae52cd
GINTOAHC <chaotingchen10@gmail.com>
2022-07-09 01:23:13 +0800

    Change Alpha layout

--- nvim/lua/user/alpha.lua
+++ nvim/lua/user/alpha.lua
@@ -4,42 +4,119 @@ if not status_ok then
 end
 
 local dashboard = require("alpha.themes.dashboard")
-dashboard.section.header.val = {
-	[[                                                 ]],
-	[[                               __                ]],
-	[[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-	[[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-	[[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-	[[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+-- dashboard.section.header.val = {
+-- 	[[                                                 ]],
+-- 	[[                               __                ]],
+-- 	[[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+-- 	[[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+-- 	[[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+-- 	[[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+-- 	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+-- }
+
+local header_1 = {
+    type = "text",
+    val = {
+        [[                               __                ]],
+        [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+        [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+        [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+        [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+        [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]]
+    },
+    opts = {
+        position = "center",
+    }
 }
 
-dashboard.section.buttons.val = {
-	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-	-- dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-	-- dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
-	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-    dashboard.button("e", "  Explore", ":NvimTreeOpen<CR>"),
-	-- dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
-	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+local header = {
+    type = "text",
+    val = {
+        [[]],
+        [[   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣭⣿⣶⣿⣦⣼⣆         ]],
+        [[    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ]],
+        [[          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷⠄⠄⠄⠄⠻⠿⢿⣿⣧⣄     ]],
+        [[           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ]],
+        [[          ⢠⣿⣿⣿⠈  ⠡⠌⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ]],
+        [[   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘⠄ ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ]],
+        [[  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ]],
+        [[ ⣠⣿⠿⠛⠄⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ]],
+        [[ ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇⠄⠛⠻⢷⣄ ]],
+        [[      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ]],
+        [[       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ]],
+        -- [[           CHAO-TING, CHEN         ]],
+        -- [[     ⢰⣶  ⣶ ⢶⣆⢀⣶⠂⣶⡶⠶⣦⡄⢰⣶⠶⢶⣦  ⣴⣶     ]],
+        -- [[     ⢸⣿⠶⠶⣿ ⠈⢻⣿⠁ ⣿⡇ ⢸⣿⢸⣿⢶⣾⠏ ⣸⣟⣹⣧    ]],
+        -- [[     ⠸⠿  ⠿  ⠸⠿  ⠿⠷⠶⠿⠃⠸⠿⠄⠙⠷⠤⠿⠉⠉⠿⠆   ]],
+        [[]],
+    },
+    opts = { position = "center" }
+}
+-- dashboard.section.buttons.val = {
+-- 	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+-- 	-- dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+-- 	-- dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+-- 	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+-- 	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+--     dashboard.button("e", "  Explore", ":NvimTreeOpen<CR>"),
+-- 	-- dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
+-- 	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+-- }
+
+local buttons = {
+    type = "group",
+    val = {
+        { type = "text", val = "Quick Links", opts = { position = "center" } },
+        { type = "padding", val = 1 },
+        dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+        -- dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+        -- dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+        dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+        dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+        dashboard.button("e", "  Explore", ":NvimTreeOpen<CR>"),
+        -- dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
+        dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+    },
+    opts = { spacing = 1 },
 }
 
-local function footer()
--- NOTE: requires the fortune-mod package to work
-	-- local handle = io.popen("fortune")
-	-- local fortune = handle:read("*a")
-	-- handle:close()
-	-- return fortune
-	return "chaotingchen10@gmail.com"
+-- local function footer()
+-- -- NOTE: requires the fortune-mod package to work
+-- 	-- local handle = io.popen("fortune")
+-- 	-- local fortune = handle:read("*a")
+-- 	-- handle:close()
+-- 	-- return fortune
+-- 	return "chaotingchen10@gmail.com"
+-- end
+
+local function time()
+    local datetime = os.date " %d-%m-%Y  %H:%M:%SS"
+    return string.format("Neovim config by CHAO-TING, CHEN\n    %s",datetime)
 end
 
-dashboard.section.footer.val = footer()
-
-dashboard.section.footer.opts.hl = "Type"
-dashboard.section.header.opts.hl = "Include"
-dashboard.section.buttons.opts.hl = "Keyword"
-
-dashboard.opts.opts.noautocmd = true
+-- dashboard.section.footer.val = footer()
+--
+-- dashboard.section.footer.opts.hl = "Type"
+-- dashboard.section.header.opts.hl = "Include"
+-- dashboard.section.buttons.opts.hl = "Keyword"
+--
+-- dashboard.opts.opts.noautocmd = true
 -- vim.cmd([[autocmd User AlphaReady echo 'ready']])
-alpha.setup(dashboard.opts)
+
+dashboard.section.footer.val = time()
+local myConfig = {
+    layout = {
+        { type = "padding", val = 1 },
+        header,
+        { type = "padding", val = 0 },
+        buttons,
+        { type = "padding", val = 1 },
+        dashboard.section.footer
+    },
+    opts = {
+        margin = 2,
+    }
+}
+
+-- alpha.setup(dashboard.opts)
+alpha.setup(myConfig)
