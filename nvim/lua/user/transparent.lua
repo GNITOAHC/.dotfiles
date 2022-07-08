5dd9a3235f94d4a496ec59c82f9f06718bc81888
GINTOAHC <chaotingchen10@gmail.com>
2022-07-09 01:37:58 +0800

    Make bufferline transparent when not opening any file

--- nvim/lua/user/transparent.lua
+++ nvim/lua/user/transparent.lua
@@ -7,14 +7,16 @@ transparent.setup({
   enable = true, -- boolean: enable transparent
   extra_groups = { -- table/string: additional groups that should be cleared
     -- In particular, when you set it to 'all', that means all available groups
-    "all",
+
     -- example of akinsho/nvim-bufferline.lua
     -- "BufferLineTabClose",
     -- "BufferlineBufferSelected",
-    -- "BufferLineFill",
+    "BufferLineFill",
     -- "BufferLineBackground",
     -- "BufferLineSeparator",
     -- "BufferLineIndicatorSelected",
   },
-  exclude = {}, -- table: groups you don't want to clear
+  exclude = {
+
+    }, -- table: groups you don't want to clear
 })
