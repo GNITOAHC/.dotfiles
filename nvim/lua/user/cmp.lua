445ce4b8f336bdae6f33ccb20185d30ccbc3a44f
GINTOAHC <chaotingchen10@gmail.com>
2022-07-09 18:07:22 +0800

    Change LSP and Tabnine's priority

--- nvim/lua/user/cmp.lua
+++ nvim/lua/user/cmp.lua
@@ -106,8 +106,8 @@ cmp.setup {
             vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
             -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
             vim_item.menu = ({
-                cmp_tabnine = "[TN]",
                 nvim_lsp = "[LSP]",
+                cmp_tabnine = "[TN]",
                 nvim_lua = "[NVIM_LUA]",
                 luasnip = "[Snippet]",
                 buffer = "[Buffer]",
