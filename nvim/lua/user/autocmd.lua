c786be256ee56290cd6dcdfaad16a45a74908d27
GINTOAHC <chaotingchen10@gmail.com>
2022-07-18 01:17:20 +0800

    Add css, html autocmd

--- nvim/lua/user/autocmd.lua
+++ nvim/lua/user/autocmd.lua
@@ -3,7 +3,7 @@ local jsGrp = api.nvim_create_augroup("JavascriptAutocmd", {})
 
 -- Javascript autocmd
 api.nvim_create_autocmd("FileType", {
-    pattern = { "javascript" },
+    pattern = { "javascript", "css", "html" },
     command = "setlocal shiftwidth=2 softtabstop=2 expandtab",
     group = jsGrp,
 })
