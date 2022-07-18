8677383d3ae6cc7fd9e965e73afb3364ed94ff49
GINTOAHC <chaotingchen10@gmail.com>
2022-07-18 14:21:36 +0800

    test alias

---
+++ .alias.zsh
@@ -0,0 +1,31 @@
+# "code [file or dir]" to open it in VScode
+alias code='/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code -r'
+
+# open moocs and moodle website in safari
+alias mooc='open http://moocs.nccu.edu.tw/'
+alias mood='open http://moodle.nccu.edu.tw/'
+
+# open incognito pages in Chrome
+alias inco='open -na "Google Chrome" --args --incognito ""'
+
+# open toggl website in Chrome
+alias toggl='browser "https://track.toggl.com/timer"'
+
+# t for tmux
+alias t='tmux'
+
+# google search in terminal type "google [what you want to search]"
+alias chrome='{read -r arr; open -a "Google Chrome" "${arr}"} <<<'
+alias browser='{read -r arr; chrome ${arr} } <<<'
+alias google='{read -r arr; browser "https://google.com/search?q=${arr}";} <<<'
+
+# make alias so that fzf with bat preview become default
+# alias fzf="fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'"
+
+# alias "FSync" to force iCloud to sync with running 'killall bird'
+alias FSync='killall bird'
+
+# Git
+alias gs="git status"
+alias gc="git commit -m"
+alias ga="git add"
