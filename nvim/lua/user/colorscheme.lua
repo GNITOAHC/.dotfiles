e109b9d96cc02aa889e392c17f65158c91c38e5e
GINTOAHC <chaotingchen10@gmail.com>
2022-11-20 13:03:09 +0800

    Add tokyonight.setup()

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,8 +1,18 @@
 local colorscheme = "tokyonight"
 
-vim.g.tokyonight_style = "night"
-vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
+local tokyo_status, tokyonight = pcall(require, "tokyonight")
+if not tokyo_status then
+    vim.notify("colorscheme " .. colorscheme .. " not found!")
+end
+
+tokyonight.setup({
+    --[[ style = "moon", ]]
+    styles = {
+        comments = { italic = true },
+        keywords = { italic = true },
+    },
+    sidebars = { "qf", "vista_kind", "terminal", "packer" },
+})
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
