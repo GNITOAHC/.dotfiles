ab24f1e76b8007f1c563dcf7223b14a8e4499f05
GNITOAHC <chaotingchen10@gmail.com>
2023-01-20 12:34:21 +0800

    feat(PowerShell): Add oh-my-posh config & pwsh profile

---
+++ powershell/user_profile.ps1
@@ -0,0 +1,19 @@
+Import-Module posh-git
+$omp_config = Join-Path $PSScriptRoot ".\negligible.omp.json"
+oh-my-posh init pwsh --config $omp_config | Invoke-Expression
+
+# Aliases
+Set-Alias l ls
+Set-Alias .. cd..
+function la () { Get-ChildItem -Force }
+
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+function touch ($filename) {
+  New-Item -Path $filename -ItemType File
+}
