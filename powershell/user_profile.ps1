a1187fae7053374dfcf3ef6a8aee67d5621741ce
GNITOAHC <chaotingchen10@gmail.com>
2023-01-29 00:20:04 +0800

    feat(powershell): Update module and alias

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -1,14 +1,30 @@
-# Import-Module posh-git
-# $omp_config = Join-Path $PSScriptRoot ".\negligible.omp.json"
-# oh-my-posh init pwsh --config $omp_config | Invoke-Expression
+# StarShip config
 $ENV:STARSHIP_CONFIG = "$HOME\.dotfiles\starship\starship.toml"
 Invoke-Expression (&starship init powershell)
 
+# Import-Module
+Import-Module -Name Terminal-Icons
+
 # Aliases
+
 Set-Alias l ls
-Set-Alias .. cd..
 function la () { Get-ChildItem -Force }
 
+Set-Alias g git 
+function gs () { git status }
+function gc () { git commit -m }
+function ga () { git add }
+function gp () { git push }
+
+Set-Alias .. cd..
+function ... () { cd ..\.. }
+function .... () { cd ..\..\.. }
+
+Set-Alias open ii
+Set-Alias grep Select-String
+
+# Tab for autocomplete
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
 # Utilities
 function which ($command) {
