8466dfc75dd77adb6899c5d1073c5347644e085e
GINTOAHC <chaotingchen10@gmail.com>
2023-03-19 22:25:50 +0800

    ci(transparent): Integrate the transparent.nvim's refactor

--- nvim/lua/user/transparent.lua
+++ nvim/lua/user/transparent.lua
@@ -1,22 +1,22 @@
 local status_ok, transparent = pcall(require, "transparent")
 if not status_ok then
-  return
+	vim.notify("transparent not found")
+	return
 end
 
+vim.g.transparent_enabled = true
+
 transparent.setup({
-  enable = true, -- boolean: enable transparent
-  extra_groups = { -- table/string: additional groups that should be cleared
-    -- In particular, when you set it to 'all', that means all available groups
+	extra_groups = { -- table/string: additional groups that should be cleared
+		-- In particular, when you set it to 'all', that means all available groups
 
-    -- example of akinsho/nvim-bufferline.lua
-    -- "BufferLineTabClose",
-    -- "BufferlineBufferSelected",
-    "BufferLineFill",
-    -- "BufferLineBackground",
-    -- "BufferLineSeparator",
-    -- "BufferLineIndicatorSelected",
-  },
-  exclude = {
-
-    }, -- table: groups you don't want to clear
+		-- example of akinsho/nvim-bufferline.lua
+		-- "BufferLineTabClose",
+		-- "BufferlineBufferSelected",
+		"BufferLineFill",
+		-- "BufferLineBackground",
+		-- "BufferLineSeparator",
+		-- "BufferLineIndicatorSelected",
+	},
+	exclude_groups = {}, -- table: groups you don't want to clear
 })
