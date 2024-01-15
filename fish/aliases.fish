875b92ddb90d2557313b1e2becb2c17167375efd
GINTOAHC <chaotingchen10@gmail.com>
2024-01-15 16:56:46 +0800

    feat: Improve `lt` command to accept different format

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -24,12 +24,16 @@ function lt --description 'alias lt=eza --tree --level $level $dir'
         eza --tree --level 2
     else if [ "$argv[2]" = "" ]
         if test -d $argv[1]
-            eza --tree --level 2 $argv[1]
+            eza --tree --level 2 $argv[1] # lt [dir]
         else
-            eza --tree --level $argv[1] .
+            eza --tree --level $argv[1] . # lt [level]
         end
     else 
-        eza --tree --level $argv[1] $argv[2]
+        if test -d $argv[2]
+            eza --tree --level $argv[1] $argv[2] # lt [level] [dir]
+        else
+            eza --tree --level $argv[2] $argv[1] # lt [dir] [level]
+        end
     end
 end
 
