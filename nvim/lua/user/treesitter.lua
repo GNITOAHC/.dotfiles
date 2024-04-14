87bb8165b8143752974ddb27df9be41349484e6a
GINTOAHC <chaotingchen10@gmail.com>
2024-04-14 23:56:22 +0800

    style

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -1,12 +1,13 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-    return
+	return
 end
 
-if (vim.g.os == 'win') then
-    require 'nvim-treesitter.install'.compilers = { "zig" }
+if vim.g.os == "win" then
+	require("nvim-treesitter.install").compilers = { "zig" }
 end
 
+-- stylua: ignore start
 local autotag_filetypes = {
     'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'tsx', 'jsx', 'rescript',
     'xml',
@@ -14,31 +15,33 @@ local autotag_filetypes = {
     'markdown',
     'glimmer','handlebars','hbs'
 }
+-- stylua: ignore end
 
-configs.setup {
+configs.setup({
+    -- stylua: ignore start
     ensure_installed = { "c", "cpp", "lua", "python", "c_sharp", "typescript", "javascript", "markdown", "scss", "fish", "vim" },
-    sync_install = false,
-    ignore_install = { "" }, -- List of parsers to ignore installing
-    autopairs = {
-        enable = true,
-    },
-    autotag = {
-        enable = true,
-        filetypes = autotag_filetypes,
-    },
-    highlight = {
-        enable = true, -- false will disable the whole extension
-        disable = { "" }, -- list of language that will be disabled
-        additional_vim_regex_highlighting = false,
-
-    },
-    indent = { enable = true, disable = { "yaml" } },
-    rainbow = {
-        enable = true,
-        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-        max_file_lines = nil, -- Do not enable for files with more than n lines, int
-        -- colors = {}, -- table of hex strings
-        -- termcolors = {} -- table of colour name strings
-    }
-}
+	-- stylua: ignore end
+	sync_install = false,
+	ignore_install = { "" }, -- List of parsers to ignore installing
+	autopairs = {
+		enable = true,
+	},
+	autotag = {
+		enable = true,
+		filetypes = autotag_filetypes,
+	},
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		disable = { "" }, -- list of language that will be disabled
+		additional_vim_regex_highlighting = false,
+	},
+	indent = { enable = true, disable = { "yaml" } },
+	rainbow = {
+		enable = true,
+		-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+		max_file_lines = nil, -- Do not enable for files with more than n lines, int
+		-- colors = {}, -- table of hex strings
+		-- termcolors = {} -- table of colour name strings
+	},
+})
