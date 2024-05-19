bb48e4faa14278acd249131644bd60695c5aadfe
GINTOAHC <chaotingchen10@gmail.com>
2024-05-19 13:25:42 +0800

    fix: Stop clangd from check proto file

--- nvim/lua/user/lsp/sever_settings.lua
+++ nvim/lua/user/lsp/sever_settings.lua
@@ -8,6 +8,7 @@ local opts = {
 M.get_clangd_opts = function()
 	local local_opts = {
 		cmd = { "clangd", "--offset-encoding=utf-16", "--fallback-style=file:~/.clang_format" },
+        filetypes = { "c", "cpp", "objc", "objcpp", "cuda" }
 	}
 	local opt = vim.tbl_deep_extend("force", local_opts, opts)
 	return opt
