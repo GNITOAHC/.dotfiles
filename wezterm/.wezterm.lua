6fe450f052af45de32c1aeee461e4912c3576d99
GNITOAHC <chaotingchen10@gmail.com>
2024-05-29 01:24:49 +0800

    feat: Add windows config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,19 +6,30 @@ local config = wezterm.config_builder()
 
 -- config.disable_default_key_bindings = true
 
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+    config.default_prog = { 'pwsh.exe', '-NoLogo' }
+end
+
 -- Color scheme
-config.color_scheme = 'Github Dark (Gogh)'
+config.color_scheme = "Github Dark (Gogh)"
 
 -- font
-config.font = wezterm.font("TerminessTTF Nerd Font Mono")
-config.font_size = 16.0
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+    config.font_size = 12.0
+end
+if wezterm.target_triple == "aarch64-apple-darwin" then -- Apple specific font
+	config.font = wezterm.font("TerminessTTF Nerd Font Mono")
+    config.font_size = 16.0
+end
+
 
 -- Appearance
 config.window_background_opacity = 0.7
 config.macos_window_background_blur = 30
 
 -- window & tab
-config.enable_tab_bar = false
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 config.window_padding = {
 	left = 20,
@@ -32,6 +43,7 @@ config.keys = {
 	{ key = "p", mods = "CMD", action = wezterm.action.ActivateCommandPalette },
 	-- Enter CopyMode
 	{ key = "v", mods = "ALT", action = wezterm.action.ActivateCopyMode },
+    { key = 't', mods = 'SHIFT|ALT', action = wezterm.action.SpawnTab("DefaultDomain") }
 }
 
 -- CopyMode
