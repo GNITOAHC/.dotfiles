9e8f8d2f0ac692533719376d75d4b1d30cbd0f2b
GNITOAHC <chaotingchen10@gmail.com>
2024-09-05 01:50:07 +0800

    refactor: Do not render markdown

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -80,9 +80,9 @@ require("lazy").setup({
 				-- Make sure to setup it properly if you have lazy=true
 				"MeanderingProgrammer/render-markdown.nvim",
 				opts = {
-					file_types = { "markdown", "Avante" },
+					file_types = { "Avante" },
 				},
-				ft = { "markdown", "Avante" },
+				ft = { "Avante" },
 			},
 		},
 	},
