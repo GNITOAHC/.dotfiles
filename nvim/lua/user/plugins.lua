2885b5b38e34428ad9b4e51654ca946de5c1a83c
GINTOAHC <chaotingchen10@gmail.com>
2024-09-04 12:54:27 +0800

    feat: Add yetone/avante.nvim

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -37,6 +37,55 @@ require("lazy").setup({
 		end,
 		dependencies = { "hrsh7th/nvim-cmp", "zbirenbaum/copilot.lua" },
 	},
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		lazy = false,
+		opts = {
+			-- add any opts here
+		},
+		-- if you want to download pre-built binary, then pass source=false. Make sure to follow instruction above.
+		-- Also note that downloading prebuilt binary is a lot faster comparing to compiling from source.
+		build = ":AvanteBuild source=false",
+        config = function()
+            require("avante").setup({
+                provider = "copilot",
+            })
+        end,
+		dependencies = {
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			"zbirenbaum/copilot.lua", -- for providers='copilot'
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = true,
+					},
+				},
+			},
+			{
+				-- Make sure to setup it properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
+	},
 
 	--[[ CMP Plugins ]]
 	"hrsh7th/nvim-cmp", -- The completion plugin
