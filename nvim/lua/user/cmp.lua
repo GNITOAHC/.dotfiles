182cd3a842166532c7d321549924d0d3ed866f99
GINTOAHC <chaotingchen10@gmail.com>
2024-09-04 12:48:13 +0800

    feat: cmp key bindings

--- nvim/lua/user/cmp.lua
+++ nvim/lua/user/cmp.lua
@@ -57,13 +57,13 @@ cmp.setup({
 			luasnip.lsp_expand(args.body) -- For `luasnip` users.
 		end,
 	},
-	mapping = {
+	mapping = cmp.mapping.preset.insert({
 		["<C-k>"] = cmp.mapping.select_prev_item(),
 		["<C-j>"] = cmp.mapping.select_next_item(),
 		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
 		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-		["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+		-- ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+		-- ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
 		["<C-e>"] = cmp.mapping({
 			i = cmp.mapping.abort(),
 			c = cmp.mapping.close(),
@@ -99,7 +99,7 @@ cmp.setup({
 		-- 	"i",
 		-- 	"s",
 		-- }),
-	},
+	}),
 	formatting = {
 		fields = { "kind", "abbr", "menu" },
 		format = function(entry, vim_item)
@@ -108,7 +108,7 @@ cmp.setup({
 			-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
 			vim_item.menu = ({
 				nvim_lsp = "[LSP]",
-                path = "[Path]",
+				path = "[Path]",
 				copilot = "[Copilot]",
 				cmp_tabnine = "[TN]",
 				nvim_lua = "[NVIM_LUA]",
@@ -147,13 +147,29 @@ cmp.setup({
 
 -- `:` cmdline setup.
 cmp.setup.cmdline(":", {
-	mapping = cmp.mapping.preset.cmdline(),
+	mapping = cmp.mapping.preset.cmdline({
+		["<C-k>"] = { c = cmp.mapping.select_prev_item() },
+		["<C-j>"] = { c = cmp.mapping.select_next_item() },
+		["<CR>"] = { c = cmp.mapping.confirm({ select = false }) },
+	}),
 	sources = cmp.config.sources({
 		{ name = "path" },
 	}, {
 		{
 			name = "cmdline",
 		},
+	}),
+})
+
+-- `/` & `?` cmdline setup.
+cmp.setup.cmdline("/", {
+	mapping = cmp.mapping.preset.insert({
+		["<C-k>"] = { c = cmp.mapping.select_prev_item() },
+		["<C-j>"] = { c = cmp.mapping.select_next_item() },
+		["<CR>"] = { c = cmp.mapping.confirm({ select = false }) },
+	}),
+	sources = cmp.config.sources({
+		{ name = "buffer" },
 	}),
 })
 
