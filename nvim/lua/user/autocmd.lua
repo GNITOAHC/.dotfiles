0d539cd659f7e675155b486eec23e5d7b4da5feb
GINTOAHC <chaotingchen10@gmail.com>
2025-06-01 23:19:22 +0800

    feat: markdown formatting setup

--- nvim/lua/user/autocmd.lua
+++ nvim/lua/user/autocmd.lua
@@ -3,12 +3,22 @@ local jsGrp = api.nvim_create_augroup("JavascriptAutocmd", {})
 
 -- Javascript autocmd
 api.nvim_create_autocmd("FileType", {
-	pattern = { "typescript", "typescriptreact", "javascript", "javascriptreact", "css", "html", "scss", "json" },
+	pattern = {
+		"typescript",
+		"typescriptreact",
+		"javascript",
+		"javascriptreact",
+		"css",
+		"html",
+		"scss",
+		"json",
+		"markdown",
+	},
 	command = "setlocal shiftwidth=2 softtabstop=2 expandtab",
 	group = jsGrp,
 })
 
-vim.cmd [[
+vim.cmd([[
     autocmd RecordingEnter * echo("Recording macro...")
     autocmd RecordingLeave * echo("Macro recorded!")
-]]
+]])
