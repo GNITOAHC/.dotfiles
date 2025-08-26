72bf448b133e547de56fe84c58cb7bab520b1a1c
GINTOAHC <chaotingchen10@gmail.com>
2025-08-27 01:21:04 +0800

    feat: Minimal nvim config

---
+++ nvim-clean/init.lua
@@ -0,0 +1,7 @@
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cmdheight = 0
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.foldlevel = 99
+vim.opt.foldmethod = "indent"
