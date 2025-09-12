d2f676ed46f0c7d7021e1e8c90c70188488c2d44
GINTOAHC <chaotingchen10@gmail.com>
2025-09-12 12:35:56 +0800

    feat: New keymap for clean neovim

--- nvim-clean/init.lua
+++ nvim-clean/init.lua
@@ -5,3 +5,15 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.foldlevel = 99
 vim.opt.foldmethod = "indent"
+
+-- Leader
+vim.api.nvim_set_keymap("", "<space>", "<Nop>", { noremap = true, silent = true })
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("n", "<leader>w", ":w<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>q", ":q<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>v", ":vsp<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>V", ":sp<CR>", { noremap = true, silent = true })
