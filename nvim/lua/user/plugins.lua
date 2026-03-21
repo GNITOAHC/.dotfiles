4b65e45b67c26c42bee3b1d46ccdbe89ee34f2a0
GINTOAHC <chaotingchen10@gmail.com>
2026-03-21 23:51:37 +0800

    chore: nvim dependencies update & luasnip installation update

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -84,7 +84,13 @@ require("lazy").setup({
 	"hrsh7th/cmp-nvim-lua",
 
 	--[[ Snippets ]]
-	"L3MON4D3/LuaSnip", --snippet engine
+	{
+		"L3MON4D3/LuaSnip",
+		-- follow latest release.
+		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		-- install jsregexp (optional!).
+		build = "make install_jsregexp",
+	},
 	"rafamadriz/friendly-snippets", -- a bunch of snippets to use
 
 	--[[ Git-conflict ]]
