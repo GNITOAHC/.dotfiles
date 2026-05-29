b9a618029b0752dc1d2c7557ac4440a01e61e9b9
GINTOAHC <chaotingchen10@gmail.com>
2026-05-29 19:07:03 +0800

    feat: launch vim.treesitter automatically

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -1,1 +1,19 @@
 require("nvim-treesitter").install({ "c", "cpp", "lua", "python", "typescript", "javascript", "markdown", "scss" })
+
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("AutoStartTreesitter", { clear = true }),
+	callback = function(args)
+		local bufnr = args.buf
+		local ft = vim.bo[bufnr].filetype
+		local lang = vim.treesitter.language.get_lang(ft)
+
+		if not lang then
+			return
+		end
+
+		local ok = pcall(vim.treesitter.language.add, lang)
+		if ok then
+			pcall(vim.treesitter.start, bufnr, lang)
+		end
+	end,
+})
