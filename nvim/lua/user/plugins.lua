c3916ffac33412e41b9a0c90f891101c47c9a29f
GINTOAHC <chaotingchen10@gmail.com>
2026-08-01 23:18:25 +0800

    refactor: installation guide link

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,3 +1,4 @@
+-- https://lazy.folke.io/installation
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
